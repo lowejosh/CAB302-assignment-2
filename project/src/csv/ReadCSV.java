cb9eec7f07aca6340d0042cd884de29f3b2ae4b1
Your name <lowejosh50@gmail.com>
2018-05-25 20:20:06 +1000

    created ReadCSV class and finished initializing a list of items from item_properties

--- project/src/csv/ReadCSV.java
+++ project/src/csv/ReadCSV.java
@@ -6,30 +6,57 @@ package csv;
  */
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import stock.Item;
+import stock.StockException;
 
 public class ReadCSV {
 
-	public static void InitialiseItems() throws IOException {
-		try {
-			FileReader reader = new FileReader("item_properties.csv");
-			BufferedReader bufferedReader = new BufferedReader(reader);
-			String line = bufferedReader.readLine();
-			if (line == null) {
-				System.out.println("The file was empty");
-			} else {
-				System.out.println("The first line of the file was:");
-				System.out.println(line);
-				bufferedReader.close();
+	public static List<Item> initialiseItems(String fileName) throws IOException {
+		List<Item> items = new ArrayList<>();
+		Path pathToFile = Paths.get(fileName);
+		
+		try (BufferedReader br = Files.newBufferedReader(
+				pathToFile, StandardCharsets.US_ASCII)) {
+			
+			String line = br.readLine();
+			
+			while (line!=null) {
+				String[] attributes = line.split(",");
+				
+				Item item = createItem(attributes);
+				
+				items.add(item);
+				line = br.readLine();
 			}
 		}
 		catch (Exception e) {
 			e.printStackTrace();
 		}
+		
+		return items;
+	}
 	
-	
-	
+	private static Item createItem(String[] data) throws StockException {
+		String name = data[0];
+		int cost = Integer.parseInt(data[1]);
+		int price = Integer.parseInt(data[2]);
+		int reorderPoint = Integer.parseInt(data[3]);
+		int reorderQuantity = Integer.parseInt(data[4]);
+		
+		try {
+			Item item = new Item(name, cost, price, reorderPoint, reorderQuantity);
+			return item;
+		} catch (Exception e) {
+			throw new StockException();
+		}
 	}
 
 }
--- project/src/csv/ReadCSVTesting.java
+++ project/src/csv/ReadCSVTesting.java
@@ -1,16 +1,24 @@
 package csv;
-
+/**
+ * TEMPORARY
+ */
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.junit.Test;
+
+import stock.Item;
 
 public class ReadCSVTesting {
 
 	@Test
 	public void test() throws IOException {
-		ReadCSV.InitialiseItems();
+		List<Item> items = ReadCSV.initialiseItems("item_properties.txt");
+		for (Item i : items) {
+			System.out.println(i);
+		}
 	}
 
 }
